20dadcb45c3117b7ab3e1ebdbd11bb09f2453507
AliBaltistani <83259990+AliBaltistani@users.noreply.github.com>
2026-04-15 15:39:17 +0500

    banner box display none:

--- resources/views/partials/homepage/slider.blade.php
+++ resources/views/partials/homepage/slider.blade.php
@@ -12,7 +12,7 @@
                         <img class="slide-bg" src="{{ asset('storage/' . $slide->image_desktop) }}" width="1180" height="569" alt="{{ $slide->title }}" />
                     @endif
                 </figure>
-                <div class="home-slide-content {{ $index % 2 !== 0 ? 'slide-content-dark' : '' }} sale-banner">
+                <div class="home-slide-content {{ $index % 2 !== 0 ? 'slide-content-dark' : '' }} sale-banner" style="display: none; background-color: {{ $slide->text_color === 'light' ? '#d7b697' : '#ceb49d' }};" >
                     <div class="row no-gutter {{ $index % 2 !== 0 ? 'bg-secondary' : 'bg-primary' }} appear-animate" data-animation-name="{{ $index % 2 !== 0 ? 'fadeInRightShorter' : 'fadeInLeftShorter' }}">
                         <div class="col-auto col-md-6 d-flex flex-column justify-content-center col-1">
                             @if($slide->subtitle)<h4 class="align-left text-uppercase mb-0 appear-animate" data-animation-name="slideInRight" data-animation-delay="400">{{ $slide->subtitle }}</h4>@endif
